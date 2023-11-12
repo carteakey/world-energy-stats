c47ef3262e86c8e28aa519393f523d4a34af92a2
Kartikey Chauhan <kartikeychauhan27@gmail.com>
2023-11-11 23:04:56 -0500

    better csv

---
+++ env/hadoop-hive-spark/duckdb-load.py
@@ -0,0 +1,36 @@
+import duckdb
+import os
+from glob import glob
+
+# Specify the path to the DuckDB database file (replace 'mydatabase.db' with your desired name)
+db_path = "energy-clean-data.db"
+
+# Specify the folder path where part files are located
+folder_path = "notebooks/clean/"
+
+# Connect to the DuckDB database (create one if it doesn't exist)
+con = duckdb.connect(db_path)
+
+# # List part files in the folder
+part_files = [f for f in glob(folder_path + "*/part-*.csv")]
+
+print(part_files)
+
+# Iterate through each part file and read it into a DuckDB table
+for part_file in part_files:
+    table_name = os.path.splitext(os.path.basename(part_file))[
+        0
+    ]  # Extract table name from file name
+    print(table_name)
+    file_path = os.path.join(folder_path, part_file)  # Full path to part file
+    print(file_path)
+    # print(
+    #     f"CREATE TABLE IF NOT EXISTS {table_name} AS SELECT * FROM read_csv_auto('{file_path}', header=true);"
+    # )
+
+    # con.execute(
+    #     f"CREATE TABLE IF NOT EXISTS {table_name} AS SELECT * FROM read_csv_auto('{file_path}', header=true);"
+    # )
+
+# Close the DuckDB connection
+con.close()
